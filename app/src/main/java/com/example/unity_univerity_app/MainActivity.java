d5da0175da0e6af2408aee6f01633b050cc5eeb5
Alazar Zewdu <alazarzewdu3@gmail.com>
2022-11-25 10:44:07 +0300

    created home screen

--- app/src/main/java/com/example/unity_univerity_app/MainActivity.java
+++ app/src/main/java/com/example/unity_univerity_app/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.unity_univerity_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash_screen);
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,home.class);
+                startActivity(intent);
+                finish();
+            }
+
+        },3000);
+
     }
 }

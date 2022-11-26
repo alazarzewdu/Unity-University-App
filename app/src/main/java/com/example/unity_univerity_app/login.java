88382732836a84c2e2384d5be911f3c108477e52
Alazar Zewdu <alazarzewdu3@gmail.com>
2022-11-26 14:29:37 +0300

    updated design

--- app/src/main/java/com/example/unity_univerity_app/login.java
+++ app/src/main/java/com/example/unity_univerity_app/login.java
@@ -4,36 +4,40 @@ import androidx.annotation.StyleableRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class login extends AppCompatActivity {
-
-    Button loginToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
-        ImageView ethiopia = findViewById(R.id.back);
-
-        ethiopia.setOnClickListener(new View.OnClickListener() {
+        Button button = findViewById(R.id.l_login);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), home.class);
-                startActivity(i);
+                LayoutInflater li = getLayoutInflater();
+                View layout = li.inflate(R.layout.toast_layout, (ViewGroup) findViewById(R.id.custom_toast_layout_id));
+                Toast toast = new Toast(getApplicationContext());
+                toast.setDuration(Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP,10,130);
+                toast.setView(layout);//setting the view of custom toast layout
+                toast.show();
             }
-        });
 
-
-
-
-
-
-    }
+    });
 }
+
+}
+

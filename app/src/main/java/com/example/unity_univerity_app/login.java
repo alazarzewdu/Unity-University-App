b580b2d5670fd3b57060333bc0c627d00cdce9cf
Alazar Zewdu <alazarzewdu3@gmail.com>
2022-12-26 13:38:37 +0300

    Added sqlite with functionality

--- app/src/main/java/com/example/unity_univerity_app/login.java
+++ app/src/main/java/com/example/unity_univerity_app/login.java
@@ -3,7 +3,12 @@ package com.example.unity_univerity_app;
 import androidx.annotation.StyleableRes;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
@@ -12,32 +17,89 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class login extends AppCompatActivity {
 
+    EditText lemail,lpassword;
+    Button l_login;
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button button = findViewById(R.id.l_login);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                LayoutInflater li = getLayoutInflater();
-                View layout = li.inflate(R.layout.toast_layout, (ViewGroup) findViewById(R.id.custom_toast_layout_id));
-                Toast toast = new Toast(getApplicationContext());
-                toast.setDuration(Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.TOP,10,130);
-                toast.setView(layout);//setting the view of custom toast layout
-                toast.show();
-            }
+        lemail = findViewById(R.id.lemail);
+        lpassword = findViewById(R.id.lpassword);
+        l_login = findViewById(R.id.l_login);
 
-    });
+
+        l_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
+
+//        Button button = findViewById(R.id.l_login);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                LayoutInflater li = getLayoutInflater();
+//                View layout = li.inflate(R.layout.toast_layout, (ViewGroup) findViewById(R.id.custom_toast_layout_id));
+//                Toast toast = new Toast(getApplicationContext());
+//                toast.setDuration(Toast.LENGTH_SHORT);
+//                toast.setGravity(Gravity.TOP,10,130);
+//                toast.setView(layout);//setting the view of custom toast layout
+//                toast.show();
+//            }
+//
+//    });
 }
+
+public void login () {
+
+        String email = lemail.getText().toString();
+        String pass = lpassword.getText().toString();
+
+        if(email.equals("") || pass.equals("")) {
+            Toast.makeText(this, "Email or Password Blank", Toast.LENGTH_SHORT).show();
+        }
+        else if (null!=checkUser(email,pass))
+        {
+            String userDb = checkUser(email,pass);
+
+            Intent i = new Intent(login.this,data.class);
+            startActivity(i);
+
+        }
+        else {
+            Toast.makeText(this, "error login", Toast.LENGTH_SHORT).show();
+        }
+
+}
+
+        public String checkUser(String email, String pass) {
+
+            SQLiteDatabase db = openOrCreateDatabase("registration", Context.MODE_PRIVATE, null);
+            Cursor cursor = db.rawQuery("select email,password from student where email = ? and password = ? ", new String[]{email,pass});
+            if(cursor.getCount() > 0) {
+                cursor.moveToFirst();
+                String email1 = cursor.getString(1);
+                String password1 = cursor.getString(2);
+                SharedPreferences.Editor sp = getSharedPreferences("email",MODE_PRIVATE).edit();
+                sp.putString("email", email1);
+                sp.apply();
+                cursor.close();
+                return email;
+            }
+            return null;
+        }
 
 }
 
--- app/src/main/java/com/example/unity_univerity_app/register.java
+++ app/src/main/java/com/example/unity_univerity_app/register.java
@@ -2,17 +2,42 @@ package com.example.unity_univerity_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class register extends AppCompatActivity {
+
+    EditText efname,elname,eemail,epassword,edepartment,esection;
+    RadioButton emale,efemale;
+    Button btn1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+        efname = findViewById(R.id.efname);
+        elname = findViewById(R.id.elname);
+        eemail = findViewById(R.id.eemail);
+        epassword = findViewById(R.id.epassword);
+        emale = findViewById(R.id.emale);
+        efemale = findViewById(R.id.efemale);
+        edepartment = findViewById(R.id.edepartment);
+        esection = findViewById(R.id.esection);
+        btn1 = findViewById(R.id.btn1);
+
 
         ImageView ethiopia = findViewById(R.id.back);
 
@@ -23,5 +48,73 @@ public class register extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insert();
+
+            }
+        });
+
+
+
+
+
+    }
+
+
+    public void insert() {
+
+        try {
+            String fname = efname.getText().toString();
+            String lname = elname.getText().toString();
+            String email = eemail.getText().toString();
+            String password = epassword.getText().toString();
+            String male = emale.getText().toString();
+            String female = efemale.getText().toString();
+            String department = edepartment.getText().toString();
+            String section = esection.getText().toString();
+
+            SQLiteDatabase db = openOrCreateDatabase("registration", Context.MODE_PRIVATE, null);
+            db.execSQL("CREATE TABLE IF NOT EXISTS student(email VARCHAR PRIMARY KEY, fname VARCHAR, lname VARCHAR, password VARCHAR, male VARCHAR, female VARCHAR, department VARCHAR, section VARCHAR)");
+
+            String sql = "insert into student(email,fname,lname,password,male,female,department,section)values(?,?,?,?,?,?,?,?)";
+            SQLiteStatement statement = db.compileStatement(sql);
+
+            statement.bindString(1,email);
+            statement.bindString(2,fname);
+            statement.bindString(3,lname);
+            statement.bindString(4,password);
+            statement.bindString(5,male);
+            statement.bindString(6,female);
+            statement.bindString(7,department);
+            statement.bindString(8,section);
+            statement.execute();
+
+            Toast.makeText(this, "Record Added", Toast.LENGTH_SHORT).show();
+
+            eemail.setText("");
+            efname.setText("");
+            elname.setText("");
+            epassword.setText("");
+            emale.setText("");
+            efemale.setText("");
+            edepartment.setText("");
+            esection.setText("");
+            efname.requestFocus();
+
+
+
+
+        }
+        catch (Exception ex) {
+
+        }
+
+
+
     }
 }
